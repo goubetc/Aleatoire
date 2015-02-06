79733415c88b1b203e43513878fca814eeaa731a
Claude Goubet <claude.goubet@e.ujf-grenoble.fr>
2015-02-06 15:44:53 +0100

    nouveau fichier: lettre.h

---
+++ lettre.h
@@ -0,0 +1,19 @@
+//#pragma once
+
+// Définitions des constantes
+#define NB_LANGUES 4
+#define NB_LETTRES 26
+typedef enum {DUTCH,ENGLISH,FRENCH,ITALIAN} langue;
+typedef struct {
+	int **tab_langue_lettre; // contient les p(L / wi) ou p(wi / L)
+	int nb_langues;
+	// char **indice_langue;
+	} table_langue;
+
+//table_langue init_tab_langue_lettre();
+
+table_langue init_tab_lettre_langue();
+
+//char *nom_langue(int l);
+
+void afficherTab(table_langue tab);
